b3aacc7515b72a1f4d90b8cdaf8a49f8225f2a20
renjujoseph <renjujoseph@gmail.com>
2016-09-14 09:45:34 -0500

    Updated and applied spell check

--- SEIcode.java
+++ SEIcode.java
@@ -4,7 +4,7 @@ import java.util.*;
 class DeDup {
 public int[] randomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,3,20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
 public static void main(String[] args) {
-  //Since the integer array is non static we need to create and object instance to use it in a statuc method.   
+  //Since the integer array is non static we need to create and object instance to use it in a static method.   
   DeDup deDup = new DeDup();  
   int[] result1 = removeDuplicatesWithLinkedHashSet(deDup.randomIntegers);
   System.out.println("RESULT 1:");
@@ -22,7 +22,7 @@ public static void main(String[] args) {
  //Positives: 
  //Preserve the input array order
  //Set by default takes care of removal of duplicate entries, so it is a better approach compared
- //to using ArrayList. In ArrayList we need to iterate through each value and seperate step needs to be 
+ //to using ArrayList. In ArrayList we need to iterate through each value and separate step needs to be 
  //considered for duplicate entry removal
  //Negative: Lines of Code is more compared to the second approach below which is available for Java 8.So if we have
  //Java compiler 8 version then we should go for option number 2
@@ -52,7 +52,7 @@ public static void main(String[] args) {
  //to create a simple approach for filtering.
  //Positives:
  //Fewer Lines of code compared to other approaches.
- //Negatives: Backward compatibility is an issue as this functionlaity is available only in Java 8.
+ //Negatives: Backward compatibility is an issue as this functionality is available only in Java 8.
  //So if we have Java compiler versions less than 8 then we should use either option 1 or 3
  private static int[] removeDuplicatesWithArrayStream(int[] inputArray) {
   //This is only compatible with Java8  
@@ -62,10 +62,10 @@ public static void main(String[] args) {
  //This is an extended approach of using Set. Here filtering of duplicates and creation of new input array
  //is completed in one single for loop.
  //Positives:
- //Hash Set removes duplicates enrty
- //Filtering of duplicates and and populating new input array is taken care in 1 single for loop.
+ //Hash Set removes duplicates entry
+ //Filtering of duplicates and populating new input array is taken care in 1 single for loop.
  //Negatives:
- //However, with this since we dont use an iterator to loop, this approach doesnt suite if we need to delete som elements in between
+ //However, with this since we dont use an iterator to loop, this approach doesnt suite if we need to delete some elements in between
  //the processing.
  //You have an extra step to copy the array elements to exclude the
  //positions which were taken by duplicate elements 
